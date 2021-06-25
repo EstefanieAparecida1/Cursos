b00528266dea3db3d80bf0b6d2bfb4fa980fa199
Estefanie <estefanieaparecida0@gmail.com>
2021-06-25 17:19:07 -0300

    final d curso sobre operadores

---
+++ src/main/java/logicos1/operadors.java
@@ -0,0 +1,29 @@
+package logicos1;
+
+public class operadors {
+
+	public static void main(String[] args) {
+//pra dar vrdadeiro precisa que ambas os lados sejam verdadeiros.
+		
+		// && e, || ou
+
+		if (10 ==10 && 200<20) {
+
+			System.out.println("verdadeiro");
+		} else {
+
+			System.out.println("falso");
+		}
+// aqui se um dos lados for verdadeiro o resultado é verdadeiro
+		
+		if (25 ==25 || 25<20) {
+
+			System.out.println("verdadeiro");
+		} else {
+
+			System.out.println("falso");
+		}
+
+	}
+
+}
