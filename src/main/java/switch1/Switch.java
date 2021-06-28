b1db5dd31a87d00e72bd52ddd3d6f6062dcca63f
Estefanie <estefanieaparecida0@gmail.com>
2021-06-28 12:00:28 -0300

    modificaçoes ao switch

--- src/main/java/switch1/Switch.java
+++ src/main/java/switch1/Switch.java
@@ -2,4 +2,25 @@ package switch1;
 
 public class Switch {
 
+	int opcoes = 2;
+
+	Switch(opcoes){
+		
+		case 1:
+			System.out.println("Abra sua conta");
+			break;
+			
+		case 2:
+			System.out.prinyln("Fatura do Cartao");
+			break;
+			
+		case 3:
+			System.out.println("Cedito Imoboliario");
+			break;
+			
+		default:
+			System.out.println("Escolha uma opcao valida");
+		
+	}
+
 }
