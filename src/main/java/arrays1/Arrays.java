732f01985721174caa8126252d46c9effcb25dee
Estefanie <estefanieaparecida0@gmail.com>
2021-06-28 11:36:04 -0300

    final curso de Arrays

---
+++ src/main/java/arrays1/Arrays.java
@@ -0,0 +1,19 @@
+package arrays1;
+
+public class Arrays {
+
+	public static void main(String[] Args) {
+
+		// Arrays string (armazena texto)serve pra criação de lista.
+		// Arrays numeros(armazena numeros) serve para criação de lista.
+
+		String[] lista_nome = { "Pedro", "Rafael", "Maria" };
+		                          //0      //1       //2
+		System.out.println(lista_nome[1]);
+		
+		int[] lista_numeros = { 29, 26, 49 };
+		
+		System.out.println(lista_numeros[1]);
+	}
+
+}
