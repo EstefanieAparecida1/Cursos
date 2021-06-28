b3e96cb921c74121dfa9f069076874dbe5b2fd3d
Estefanie <estefanieaparecida0@gmail.com>
2021-06-28 11:05:40 -0300

    fim do curso de loops

---
+++ src/main/java/loops1/While.java
@@ -0,0 +1,32 @@
+package loops1;
+
+public class While {
+	
+	
+	 public static void  main(String [] args) {
+ //WHILE - enquanto.
+		 
+		 int contador = 0;
+		 
+		 while(contador <= 5) {
+			 System.out.println(contador);
+			contador += 1;} 
+		
+		 
+//FOR -			 
+		 
+		 for(int contador1 = 0; contador1 <= 10; contador1+=2) {
+			 System.out.println(contador1);
+			 
+		 }
+			 
+	 }
+	 
+		 
+ }		
+		 
+	
+
+
+	
+
