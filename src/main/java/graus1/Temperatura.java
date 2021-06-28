804b1f9754717fccb8e6737bdf400d503e05b497
Estefanie <estefanieaparecida0@gmail.com>
2021-06-28 17:19:59 -0300

    final da atividade de temperatura

--- src/main/java/graus1/Temperatura.java
+++ src/main/java/graus1/Temperatura.java
@@ -1,24 +1,47 @@
 package graus1;
 
+import java.util.Scanner;
+
 public class Temperatura {
-	
+
 	public static void main(String[] Args) {
-
-		double Celsius = 30, Kelvin, Reaumur, Rankine, Fahrenheit;
-
-		Fahrenheit = Celsius * 1.8 + 32;
-		Kelvin = Celsius + 273.15;
-		Rankine = Celsius * 1.8 + 32 + 459.67;
-		Reaumur = Celsius * 0.8;
-
-		System.out.println("A temperatura em Fahrenheit é:" +Fahrenheit);
-		System.out.println("A temperatura em Kelvin é: " + Kelvin);
-		System.out.println("A temperatura em Reaumur é: " + Rankine);
-		System.out.println("A temperatura em Rankine é: " + Reaumur);
+		
+		double Celsius2;
+				
+		Scanner ler = new Scanner(System.in);
+		
+    	System.out.println("Temperatura");
+		
+		System.out.println("insira os graus Celsius: ");
+		Celsius2 = ler.nextDouble();
+		
+		String Resposta;
+		
+		System.out.println("Para qual escala voce quer? ");
+	    Resposta = ler.next();
+	    
+	
+		switch(Resposta){
+		
+		case ("Fahrenheit"):
+			System.out.println("A temperatura em Fahrenheit é:" +( Celsius2 * 1.8 + 32));
+			break;
+			
+		case ("Kelvin"):
+			System.out.println("A temperatura em Kelvin é: " + (Celsius2 +273.15));
+			break;
+			
+		case ("Rankine"):
+			System.out.println("A temperatura em Rankine é: " +  (Celsius2 * 1.8 + 32 + 459.67));
+			break;
+			
+		case ("Reaumur"):
+			System.out.println("A temperatura em Reaumur é: " +  (Celsius2 * 0.8));
+			break;
+			
+		
+		}
 	
 	}
 
 }
-
-
-
